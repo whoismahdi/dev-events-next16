dbdc024c94d6b236bc05b4e8f44587d81f7d3ff5
Mahdi <mahdiseda15@gmail.com>
2025-11-29 02:23:00 +0330

    Add SpeedInsights import to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
